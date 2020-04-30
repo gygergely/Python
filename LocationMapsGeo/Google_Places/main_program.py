bfcf9ddf9c79e5d61426de51fc603378ea0764b7
gygergely <gyetvaigergely@gmail.com>
2020-04-30 21:40:36 +0200

    moved g place api to a separate file

--- LocationMapsGeo/Google_Places/main_program.py
+++ LocationMapsGeo/Google_Places/main_program.py
@@ -1,51 +1,10 @@
-import requests
 import json
-import time
 import csv
 import math
 from timeit import default_timer as timer
+import LocationMapsGeo.Google_Places.google_places_api as google_api
 
 CONFIG_FILE = 'config.json'
-
-
-class GooglePlacesAPIObj(object):
-    def __init__(self, api_key):
-        super(GooglePlacesAPIObj, self).__init__()
-        self.apiKey = api_key
-
-    def search_places_by_coordinate(self, location, radius, types):
-        endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-        places_from_api = []
-        params = {
-            'location': location,
-            'radius': radius,
-            'types': types,
-            'key': self.apiKey
-        }
-        res = requests.get(endpoint_url, params=params)
-        results = json.loads(res.content)
-        places_from_api.extend(results['results'])
-
-        time.sleep(2)
-
-        while "next_page_token" in results:
-            params['pagetoken'] = results['next_page_token'],
-            res = requests.get(endpoint_url, params=params)
-            results = json.loads(res.content)
-            places_from_api.extend(results['results'])
-            time.sleep(2)
-        return places_from_api
-
-    def get_place_details(self, place_id, fields):
-        endpoint_url = "https://maps.googleapis.com/maps/api/place/details/json"
-        params = {
-            'placeid': place_id,
-            'fields': ",".join(fields),
-            'key': self.apiKey
-        }
-        res = requests.get(endpoint_url, params=params)
-        place_details = json.loads(res.content)
-        return place_details
 
 
 def get_api_key():
@@ -182,7 +141,7 @@ if __name__ == '__main__':
     # meters to move the search circle midpoint (move on latitude or longitude
     meters_to_increase_search = 420
 
-    api = GooglePlacesAPIObj(get_api_key())
+    api = google_api.GooglePlacesAPIObj(get_api_key())
 
     search_types = ['restaurant', 'bakery', 'cafe']
     extended_id_list = list()
